b5624f7051924f5f0f0c77405eb2730068f202cb
Askouiq <157696149+Askouiq@users.noreply.github.com>
2024-01-25 17:51:33 +0100

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,7 +1,7 @@
 #pip install pycryptodome  , It works only v3.11.5 Above.
 import random ,base64,codecs,zlib,sys;py=""
 sys.setrecursionlimit(1000000000) 
-
+ 
  
 def genrataPassOSN():
     if utils.get_gdbserver_type() == utils.GDBSERVER_QEMU:
